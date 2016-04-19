4bc25398a3ed7620c6403901d2790fe304e18392
Claudio Montoya <cm@paybook.com>
2016-04-19 01:32:50 -0500

    First version Paybook Lite golang Based on Beego SQLite3 SignUp and SignIn support

--- main.go
+++ main.go
@@ -13,8 +13,6 @@ var globalSessions *session.Manager
 
 func init() {
 
-	// globalSessions, _ = session.NewManager("memory", `{"cookieName":"gosessionid", "enableSetCookie,omitempty": true, "gclifetime":3600, "maxLifetime": 3600, "secure": false, "sessionIDHashFunc": "sha1", "sessionIDHashKey": "", "cookieLifeTime": 3600, "providerConfig": ""}`)
-	// go globalSessions.GC()
 }
 
 func main() {
